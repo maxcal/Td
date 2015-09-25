0c38cd54e0ba38d12bb45458f4748958e81136a4
Max Calabrese <max.calabrese@ymail.com>
2015-09-25 17:33:10 +0200

    add view overrides for foundation

---
+++ app/presenters/refinery/pages/zurb_menu_presenter.rb
@@ -0,0 +1,10 @@
+# see https://raw.githubusercontent.com/milkfarm/refinery-foundation/
+module Refinery
+  module Pages
+    class ZurbMenuPresenter < MenuPresenter
+      self.css = 'large-9 columns'
+      self.list_tag_css = 'right button-group'
+      self.link_tag_css = 'button'
+    end
+  end
+end
